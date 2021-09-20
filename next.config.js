1e2de5f5cd15d53e868a92f9dc26d1d301b78153
madarsbiss <madars.biss@gmail.com>
2021-09-20 23:48:08 +0300

    fx rename

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  images: {
-    domains: ["openweathermap.org"],
-  },
-};
+// module.exports = {
+//   images: {
+//     domains: ["openweathermap.org"],
+//   },
+// };
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function WeatherApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default WeatherApp;
